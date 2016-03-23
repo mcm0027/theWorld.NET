146a614a71ae7fdfe74ee4ca3ff4f7d9f74cc58f
Matt Morris <mcm0027@yahoo.com>
2016-03-23 17:33:55 -0500

    add jQuery and toggle button for sidenav

--- src/TheWorld/wwwroot/scripts/index.js
+++ src/TheWorld/wwwroot/scripts/index.js
@@ -1,11 +1,30 @@
-﻿var ele = document.getElementById("username");
-ele.innerHTML = "Matt Morris";
+﻿(function() {
 
-var main = document.getElementById("main");
-main.onmouseenter = function () {
-    main.style = "background-color: #888;";
-};
+    //var ele = $("#username");
+    //ele.text("matt morris");
 
-main.onmouseleave = function () {
-    main.style = "";
-}
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style["background-color"] = "#888;";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style["background-color"] = "";
+    //});
+
+    //var menuitems = $("ul.menu li a");
+    //menuitems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        };
+    });
+})();
