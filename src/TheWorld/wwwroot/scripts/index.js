a07058c64a9318bf37b73eb688d780869bd168ad
Matt Morris <mcm0027@yahoo.com>
2016-03-23 15:46:54 -0500

    add basic styling and markup

---
+++ src/TheWorld/wwwroot/scripts/index.js
@@ -0,0 +1,11 @@
+﻿var ele = document.getElementById("username");
+ele.innerHTML = "Matt Morris";
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style = "background-color: #888;";
+};
+
+main.onmouseleave = function () {
+    main.style = "";
+}
