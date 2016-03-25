8c585c38d81e82b79e313987e101941d43d31b5f
Matt Morris <mcm0027@yahoo.com>
2016-03-25 12:06:57 -0500

    add methods for adding Stops to Trips

--- src/TheWorld/Controllers/Api/StopController.cs
+++ src/TheWorld/Controllers/Api/StopController.cs
@@ -42,7 +42,37 @@ namespace TheWorld.Controllers.Api
                 Response.StatusCode = (int)HttpStatusCode.BadRequest;
                 return Json("Error occured finding trip name");
             }
+        }
 
+        public JsonResult Post(string tripName, [FromBody]StopViewModel vm)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Map to the Entity
+                    var newStop = Mapper.Map<Stop>(vm);
+                    //Look up Geocoordinates
+
+                    //save tot he database
+                    _repository.AddStop(tripName, newStop);
+
+                    if(_repository.SaveAll())
+                    {
+                        Response.StatusCode = (int)HttpStatusCode.Created;
+                        return Json(Mapper.Map<StopViewModel>(newStop));
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError("Failed to save new stop", ex);
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json("Failed to save new stop");
+            }
+
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return Json("Validation failed on new stop");
         }
     }
 }
